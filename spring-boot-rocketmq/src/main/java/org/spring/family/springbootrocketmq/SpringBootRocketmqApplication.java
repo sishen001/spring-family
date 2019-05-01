b08ef2188edfc966b6d68613b57a58530ec74c68
dafeige <364583707@qq.com>
2019-05-01 23:36:08 +0800

    spring-family

---
+++ spring-boot-rocketmq/src/main/java/org/spring/family/springbootrocketmq/SpringBootRocketmqApplication.java
@@ -0,0 +1,13 @@
+package org.spring.family.springbootrocketmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRocketmqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootRocketmqApplication.class, args);
+    }
+
+}
