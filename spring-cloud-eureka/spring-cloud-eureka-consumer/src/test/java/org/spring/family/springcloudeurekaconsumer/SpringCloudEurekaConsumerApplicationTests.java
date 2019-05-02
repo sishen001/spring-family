377c58fd4355e859a40f44e0120c9e9a9676916f
dafeige <364583707@qq.com>
2019-05-02 17:58:36 +0800

    完成springcloud eureka 简单学习

--- spring-cloud-eureka/spring-cloud-eureka-consumer/src/test/java/org/spring/family/springcloudeurekaconsumer/SpringCloudEurekaConsumerApplicationTests.java
+++ spring-cloud-eureka/spring-cloud-eureka-consumer/src/test/java/org/spring/family/springcloudeurekaconsumer/SpringCloudEurekaConsumerApplicationTests.java
@@ -1,4 +1,4 @@
-package org.spring.family.springcloudeureka;
+package org.spring.family.springcloudeurekaconsumer;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SpringCloudEurekaApplicationTests {
+public class SpringCloudEurekaConsumerApplicationTests {
 
     @Test
     public void contextLoads() {
